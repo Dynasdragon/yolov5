f3e737da54a037fc04661eb95c1bdfa97adf4fb8
Eddy Tran <Eddy.tran453@hotmail.com>
2024-03-21 23:55:43 -0400

    clean up code

--- dataset/README.roboflow.txt
+++ dataset/README.roboflow.txt
@@ -1,16 +1,31 @@
 
-License Plates - v3 Original License Plates
+License Plates - v1 2023-12-05 3:51am
 ==============================
 
-This dataset was exported via roboflow.ai on June 29, 2022 at 6:34 AM GMT
+This dataset was exported via roboflow.com on December 5, 2023 at 8:52 AM GMT
 
-It includes 350 images.
-Plates are annotated in YOLOv5 Oriented Object Detection format.
+Roboflow is an end-to-end computer vision platform that helps you
+* collaborate with your team on computer vision projects
+* collect & organize images
+* understand and search unstructured image data
+* annotate, and create datasets
+* export, train, and deploy computer vision models
+* use active learning to improve your dataset over time
+
+For state of the art Computer Vision training notebooks you can use with this dataset,
+visit https://github.com/roboflow/notebooks
+
+To find over 100k other datasets and pre-trained models, visit https://universe.roboflow.com
+
+The dataset includes 839 images.
+Plates are annotated in YOLO v5 PyTorch format.
 
 The following pre-processing was applied to each image:
 * Auto-orientation of pixel data (with EXIF-orientation stripping)
 * Resize to 416x416 (Stretch)
 
-No image augmentation techniques were applied.
+The following augmentation was applied to create 3 versions of each source image:
+* Random exposure adjustment of between -25 and +25 percent
+* Random Gaussian blur of between 0 and 2 pixels
 
 
--- detect.py
+++ detect.py
@@ -56,56 +56,6 @@ from utils.general import (LOGGER, Profile, check_file, check_img_size, check_im
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 from utils.torch_utils import select_device, smart_inference_mode
 
-def my_function(image):
-        
-    # convert the image to grayscale and flip the foreground
-    # and background to ensure foreground is now "white" and
-    # the background is "black"
-    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #gray = cv2.bitwise_not(gray)
-    
-    # threshold the image, setting all foreground pixels to
-    # 255 and all background pixels to 0
-    thresh = cv2.threshold(image, 0, 255,
-        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    
-    # grab the (x, y) coordinates of all pixel values that
-    # are greater than zero, then use these coordinates to
-    # compute a rotated bounding box that contains all
-    # coordinates
-    coords = np.column_stack(np.where(thresh > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    
-    # the `cv2.minAreaRect` function returns values in the
-    # range [-90, 0); as the rectangle rotates clockwise the
-    # returned angle trends to 0 -- in this special case we
-    # need to add 90 degrees to the angle
-    if angle < -45:
-        angle = -(90 + angle)
-    
-    # otherwise, just take the inverse of the angle to make
-    # it positive
-    else:
-        angle = -angle
-    
-    # rotate the image to deskew it
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h),
-        flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    
-    # draw the correction angle on the image so we can validate it
-    #cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
-    #    (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    
-    # show the output image
-    print("[INFO] angle: {:.3f}".format(angle))
-    cv2.imshow("Input", image)
-    cv2.imshow("Rotated", rotated)
-    return rotated
-    cv2.waitKey(0)
-
 @smart_inference_mode()
 def run(
         weights=ROOT / 'yolov5s.pt',  # model path or triton URL
@@ -262,7 +212,7 @@ def run(
                     save_obj = True
                     if save_obj:
                         
-                        if int(cls) == 1:
+                        if int(cls) == 15:
                             filepath = str(save_dir / f'lp{j}{p.name}')
                             x,y,w,h=int(xyxy[0]), int(xyxy[1]), int(xyxy[2] - xyxy[0]),   int(xyxy[3] - xyxy[1])
                             img_ = im0.astype(np.uint8)
@@ -299,7 +249,6 @@ def run(
                             #hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
                             #msk = cv2.inRange(hsv, np.array([0, 0, 123]), np.array([179, 255, 255]))
                             #cv2.imwrite(filepath, (msk))
-
                             text = pytesseract.image_to_string(filepath, config='--psm 8')
                             clean_string = [s for s in text if s.isalnum()]
 
